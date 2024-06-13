a181f5b65b013994cfe779fe3848dcfb0085f0cb
ezizovmekan <liguoqiang@stu.xidian.edu.cn>
2024-06-13 19:10:17 +0800

    added isPalindrome function into arraysum.c file and also added .gitignore file to the project

--- arraysum.c
+++ arraysum.c
@@ -24,7 +24,44 @@ int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
     return returnSize;
 }
 
+// palindrome
+bool isPalindrome(){
+    int x;
+    bool res = false;
+    int arr[10];
+    int len = 0, i = 0, j;
 
+    // san belgilerini massiwa salmak
+    if (x % 10 != 0) {
+        while (x > 0) {
+            arr[i] = x % 10;
+            x /= 10;
+            i++;
+        }
+        len = i;
+        int mid = len / 2;
+
+        // barlag gechirmek
+        if (x < 0)
+            res = false;
+        else
+            for (i = 0, j = len - 1; i <= mid, j >= mid; i++, j--) {
+                if (arr[i] == arr[j])
+                    res = true;
+                else {
+                    res = false;
+                    break;
+                }
+            }
+    } else if (x == 0)
+        res = true;
+    else
+        res = false;
+
+    return res;
+}
+
+// prime number
 int isPrime(){
     int n;
     bool prime = true;
